443751d5699f434e48f7885323a85567ffb93669
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2024-07-21 13:15:14 +0100

    committing new updates

--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 function CtaSection() {
   return (
     <section className='section cta__section'>
-        <div className="container cta--container">
+        <div className="container">
+          <div className="cta--container">
             <h4 className="cta--heading">Lorem ipsum dolor adipisicing ipsum dolor sit elit.</h4>
             <p className="cta--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas ad temporibus. Error officiis dipisci eos recusandae accusamus sed mollitia voluptatum tempora.</p>
 
             <Link to="/" className='cta--btn btn'>Let's Talk</Link>
+          </div>
         </div>
     </section>
   )
